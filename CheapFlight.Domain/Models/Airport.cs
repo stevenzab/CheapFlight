b076e64f3476c5bb26c32d4b2828eefff9e9fd2b
stevenzab <steven.zabandith@epitech.eu>
2026-03-30 16:02:22 +0200

    [ADD] airport model

---
+++ CheapFlight.Domain/Models/Airport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheapFlight.Domain.Models
+{
+    public class Airport
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
