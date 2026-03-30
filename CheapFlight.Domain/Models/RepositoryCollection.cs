58ede23b77ab299cac6a024a240098c9601eeaa7
stevenzab <steven.zabandith@epitech.eu>
2026-03-30 16:29:59 +0200

    [ADD] repositoryCollection

---
+++ CheapFlight.Domain/Models/RepositoryCollection.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CheapFlight.Domain.Models
+{
+    public class RepositoryCollection
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime Updated { get; set; }
+    }
+}
+}
