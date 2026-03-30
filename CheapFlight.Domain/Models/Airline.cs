e129ca438ac00e241511cfd7055ae918c248b6e0
stevenzab <steven.zabandith@epitech.eu>
2026-03-30 16:02:12 +0200

    [ADD] Airline model

---
+++ CheapFlight.Domain/Models/Airline.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheapFlight.Domain.Models
+{
+    public class Airline
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
