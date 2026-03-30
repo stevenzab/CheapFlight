10a2dc1e0f976d7b77ce42c88cc3bd7c8e386739
stevenzab <steven.zabandith@epitech.eu>
2026-03-30 14:59:39 +0200

    [ADD] infrastructure reference

--- CheapFlight.API/Program.cs
+++ CheapFlight.API/Program.cs
@@ -1,12 +1,15 @@
+using CheapFlight.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddProblemDetails();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
--- CheapFlight.Infrastructure/DependencyInjection.cs
+++ CheapFlight.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,26 @@
-﻿namespace CheapFlight.Infrastructure
-{
-    public class DependencyInjection
-    {
+﻿using CheapFlight.Domain.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
+namespace CheapFlight.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mongodbSettings = configuration
+               .GetSection("MongoDB")
+               .Get<MongoSettings>();
+
+            if (mongodbSettings == null)
+                throw new InvalidOperationException("MongoDB settings are not configured properly.");
+
+            var mongoClient = new MongoClient(mongodbSettings.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(mongodbSettings.DatabaseName);
+
+            services.AddSingleton(mongoDatabase);
+            return services;
+        }
     }
 }
