b694b7f3b61d612c37a6551deca148fa1bf0b5d6
stevenzab <steven.zabandith@epitech.eu>
2026-03-30 16:02:39 +0200

    [ADD] flight model

---
+++ CheapFlight.Domain/Models/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheapFlight.Domain.Models
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; } = string.Empty;
+        public string AirlineCode { get; set; } = string.Empty;
+        public string DepartureAirport { get; set; } = string.Empty;
+        public string ArrivalAirport { get; set; } = string.Empty;
+
+        public DateTime DepartureTime { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
